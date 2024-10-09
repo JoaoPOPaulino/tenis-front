4e2c118f3c9c54deed1bc7b67fd51a6a2c55bf92
Jp <joaopoliveirapaulino@gmail.com>
2024-10-09 20:07:19 -0300

    Tema modificado

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Tenis } from '../models/tenis.model';
 import { Observable } from 'rxjs';
+import { Tenis } from '../models/tenis.model';
 import { Usuario } from '../models/usuario.model';
 
 @Injectable({
