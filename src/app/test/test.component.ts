c56c9627812df34256e1232a2be77b474cc36d87
oluizoliveira997 <oliveiraluiz@unitins.br>
2024-10-11 08:54:25 -0300

    Arrumando algumas coisas, implementando o paper-kit

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+    <div class="container">
+      <button class="btn btn-primary">Teste Paper Kit</button>
+    </div>
+  `
+})
+export class TestComponent {}
