93704c27e17148dedbf69b493f9583d70a9db2d8
oluizoliveira997 <oliveiraluiz@unitins.br>
2024-10-16 23:14:54 -0300

    rapiao

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,17 +1,33 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Usuario } from '../models/usuario.model';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class UsuarioService {
-  private baseUrl = 'http://localhost:8080/usuarios';
+  private apiUrl = 'http://localhost:8080/usuarios';
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getUsuarios(): Observable<Usuario[]> {
-    return this.httpClient.get<Usuario[]>(this.baseUrl);
+    return this.http.get<Usuario[]>(this.apiUrl);
+  }
+
+  getUsuario(id: number): Observable<Usuario> {
+    return this.http.get<Usuario>(`${this.apiUrl}/${id}`);
+  }
+
+  createUsuario(usuario: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>(this.apiUrl, usuario);
+  }
+
+  updateUsuario(id: number, usuario: Usuario): Observable<Usuario> {
+    return this.http.put<Usuario>(`${this.apiUrl}/${id}`, usuario);
+  }
+
+  deleteUsuario(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
