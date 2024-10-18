62775e017376e0518f4f74c82c9dee6a7acf1600
oluizoliveira997 <oliveiraluiz@unitins.br>
2024-10-18 19:02:37 -0300

    Tá com erros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideHttpClient} from '@angular/common/http';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 
-// Definindo as rotas
-const routes: Routes = [
-  { path: '', component: HomeComponent }
-];
+import { AppRoutingModule } from './app.routes';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { EstadoListComponent } from './components/estado/estado-list/estado-list.component';
+import { ProdutoListComponent } from './components/produto/produto-list/produto-list.component';
+import { ProdutoFormComponent } from './components/produto/produto-form/produto-form.component';
+import { UsuarioListComponent } from './components/usuario/usuario-list/usuario-list.component';
+import { FornecedorListComponent } from './components/fornecedor/fornecedor-list/fornecedor-list.component';
+import { MarcaListComponent } from './components/marca/marca-list/marca-list.component';
+import { TenisListComponent } from './components/tenis/tenis-list/tenis-list.component';
 
 @NgModule({
   declarations: [
-    HomeComponent // Certifique-se de ter registrado o seu componente Home
+    AppComponent,
+    HomeComponent,
+    EstadoListComponent,
+    ProdutoListComponent,
+    ProdutoFormComponent,
+    UsuarioListComponent,
+    FornecedorListComponent,
+    MarcaListComponent,
+    TenisListComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule, // Necessário para animações do Angular Material
-    MatToolbarModule, // Para barra de ferramentas
-    MatButtonModule,  // Para botões
-    MatCardModule,    // Para cards
-    RouterModule.forRoot(routes) // Para configuração das rotas
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [HomeComponent] // Defina o componente principal a ser carregado
+  providers: [
+    provideHttpClient() // Substitui o HttpClientModule
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
